6b4819d0393275c4e800055c8ba4463c81b1a1e7
MauriceGit <maurice.tollmien@gmail.com>
2015-10-02 17:21:07 +0200

    blubb

--- Data/Expr/Proposition/Substitute.hs
+++ Data/Expr/Proposition/Substitute.hs
@@ -1,6 +1,6 @@
 module Data.Expr.Proposition.Substitute where
 
-import Data.List      (union)
+import Data.List      (union, nub)
 
 import Data.Expr.Proposition.Types
 
@@ -13,6 +13,11 @@ substVars :: VarEnv -> Expr -> Expr
 substVars env e = undefined
 
 freeVars :: Expr -> [Ident]
-freeVars  = undefined
+freeVars e = freeVars' e
+	where
+	freeVars' (Lit _) = []
+	freeVars' (Var i) = [i]
+	freeVars' (Unary _ e) = freeVars' e
+	freeVars' (Binary _ e1 e2) = freeVars' e1 `union` freeVars' e2
 
--- ----------------------------------------
+-- freeVars (either (error.show) id  (parse' "a && true || b => c"))
