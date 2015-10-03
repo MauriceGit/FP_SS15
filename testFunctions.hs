2d45c6e6752244092f6729cebf720b9b50a020f4
MauriceGit <maurice.tollmien@gmail.com>
2015-10-03 11:34:58 +0200

    ein paar einfache Testfunktionen

---
+++ testFunctions.hs
@@ -0,0 +1,100 @@
+
+
+-- foldr Implementierung
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f e [] = e
+foldr' f e (x:xs) = f x (foldr' f e xs)
+
+-- foldl Implementierung
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' f e [] = e
+foldl' f e (x:xs) = f (foldl' f e xs) x
+
+-- map mit foldr
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr' (\ x xs -> (f x) : xs) [] 
+
+-- map mit foldl
+map'' :: (a -> b) -> [a] -> [b]
+map'' f = foldl' (\ xs x -> (f x) :xs) [] 
+
+-- reverse mit foldr
+reverse' :: [a] -> [a]
+reverse' = foldr' (\ x xs -> xs ++ [x]) []
+
+-- reverse mit foldl
+reverse'' :: [a] -> [a]
+reverse'' = foldl' (\ xs x -> xs ++ [x]) []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
