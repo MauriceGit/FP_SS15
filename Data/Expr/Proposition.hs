21fd1d53211320a4cde481abd7db1a057268148f
MauriceGit <maurice.tollmien@gmail.com>
2015-10-03 15:35:15 +0200

    Evil Witchcraft Visitor Shit

--- Data/Expr/Proposition.hs
+++ Data/Expr/Proposition.hs
@@ -3,8 +3,9 @@
 module Data.Expr.Proposition
        ( module Data.Expr.Proposition.Types
        , module Data.Expr.Proposition.Constr
-       , module Data.Expr.Proposition.Eval
-       , module Data.Expr.Proposition.Substitute
+       -- , module Data.Expr.Proposition.Eval
+       -- , module Data.Expr.Proposition.Substitute
+       , module Data.Expr.Proposition.Visitors
        , module Data.Expr.Proposition.Proof
        , module Data.Expr.Proposition.Parse
        , module Data.Expr.Proposition.Visit
@@ -13,8 +14,9 @@ where
 
 import Data.Expr.Proposition.Types
 import Data.Expr.Proposition.Constr
-import Data.Expr.Proposition.Eval
-import Data.Expr.Proposition.Substitute
+-- import Data.Expr.Proposition.Eval
+-- import Data.Expr.Proposition.Substitute
+import Data.Expr.Proposition.Visitors
 import Data.Expr.Proposition.Proof
 import Data.Expr.Proposition.Parse
 import Data.Expr.Proposition.Visit
--- Data/Expr/Proposition/Visitors.hs
+++ Data/Expr/Proposition/Visitors.hs
@@ -13,21 +13,36 @@ import qualified Data.Set as S
 
 type Idents = Set Ident
 
+freeVarsVisitor :: Visitor Idents
+freeVarsVisitor = V {vLit = const S.empty, vVar = S.singleton, vUnary = (\ f -> id), vBinary = (\ f -> S.union)}
+
 freeVars :: Expr -> Idents
-freeVars
-  = visit $
-    undefined
+freeVars = visit freeVarsVisitor
+
+-- Testfall:
+-- freeVars (either (error.show) id  (parse' "a && true || b => c"))
     
 type VarEnv = [(Ident, Expr)]
 
+substVarsVisitor :: VarEnv -> Visitor Expr
+substVarsVisitor env = V {vLit = Lit . id, vVar = (\ i -> maybe (Var i) id (lookup i env)), vUnary = Unary . id, vBinary = Binary . id} 
+-- Binary . id  <===>  (\ f e1 e2 -> Binary f e1 e2)
+-- Das Gleiche gilt für Unary und Lit
+
 substVars :: VarEnv -> Expr -> Expr
-substVars env
-  = visit $
-    undefined
+substVars env = visit $ substVarsVisitor env
+
+-- Testfall:
+-- substVars [("a", Lit False)] ( either (error.show) id (parse' "a && b"))
+
+evalVisitor :: Visitor Bool
+evalVisitor = V {vLit = id, vVar = (\ x -> error "There shouldn't be variables any more..."), vUnary = (\ f e -> (mf1 f) e), vBinary = (\ f e1 e2 -> (mf2 f) e1 e2)}
 
 eval :: Expr -> Bool
-eval
-  = visit $
-    undefined
+eval = visit evalVisitor
+
+-- Testfall:
+-- eval (either (error.show) id  (parse' "false && true && false => true"))
+-- eval (either (error.show) id  (parse' "false && true || true => false"))
 
 -- ----------------------------------------
