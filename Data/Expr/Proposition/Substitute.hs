0ea91afae423b22bf1c1ff1681cd738da66218ef
MauriceGit <maurice.tollmien@gmail.com>
2015-10-03 12:49:57 +0200

    lökjsdf

--- Data/Expr/Proposition/Substitute.hs
+++ Data/Expr/Proposition/Substitute.hs
@@ -13,21 +13,10 @@ type VarEnv = [(Ident, Expr)]
 substVars :: VarEnv -> Expr -> Expr
 substVars env e = substVars' e 
     where
-    
         substVars' (Lit e) = Lit e
-        -- Mit selbstgebautem: maybe
-        -- substVars' (Var i) = lookup' (lookup i env) (Var i)
         substVars' (Var i) = maybe (Var i) id (lookup i env)
         substVars' (Unary f e) = Unary f (substVars' e)
         substVars' (Binary f e1 e2) = Binary f (substVars' e1) (substVars' e2)
-        
-        -- Die Funktion ist optional, tut das Gleiche, wie maybe
-        -- maybe wendet das zweite Argument auf das dritte Argument an,
-        -- sofern dieses Just a ist. Und gibt sonst das erste zurück.
-        -- Also das Gleich, wie lookup'.
-        lookup' :: Maybe Expr -> Expr -> Expr
-        lookup' (Just i) _ = i
-        lookup' Nothing i = i
 
 -- Testaufruf:
 -- substVars [("a", Lit False)] ( either (error.show) id (parse' "a && b"))
