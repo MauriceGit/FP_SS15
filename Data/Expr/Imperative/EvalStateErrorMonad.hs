917f88a943ba0976c1441a8d4ec92ba68985aedc
MauriceGit <maurice.tollmien@gmail.com>
2015-10-07 14:20:09 +0200

    Ellen is furious.

--- Data/Expr/Imperative/EvalStateErrorMonad.hs
+++ Data/Expr/Imperative/EvalStateErrorMonad.hs
@@ -2,23 +2,28 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module Data.Expr.ArithmLogic.EvalReaderErrorMonad where
+module Data.Expr.Imperative.EvalStateErrorMonad where
+
+import Prelude hiding (lookup)
 
 import Control.Applicative (Applicative(..))
 import Control.Monad
 import Control.Monad.Except
-import Control.Monad.Reader
+import Control.Monad.State
 
-import Data.Expr.ArithmLogic.Types
+import Data.Expr.Imperative.Types
+import Data.Expr.Imperative.Constr
 import Data.Pretty
 
-import Data.Map (Map)
-import qualified Data.Map as M
+import qualified Data.List as L
+import qualified Data.Map  as M
 
 -- ----------------------------------------
--- simple expression evaluation in monadic form
---
--- evaluates expressions with free variables and local let bindings
+-- evaluation of simple imperative programs
+-- 
+-- this is an example for using the state monad
+-- the evaluator is derived out of the reader/error
+-- evaluator in ArithmLogic dir
 
 -- ----------------------------------------
 --
@@ -74,75 +79,78 @@ instance (Pretty a) => Pretty (ResVal a) where
 
 -- ----------------------------------------
 
-type Env = Map Ident Value
-
-newtype Result a = RR { unRR :: Env -> ResVal a }
+newtype Result a = RT { runResult :: Store -> (ResVal a, Store) }
 
 instance Functor Result where
-  {--
-  fmap f (RR a) = RR (\ env -> 
-                        let y = a env in 
-                        do 
-                            z <- y
-                            return (f z)
-                     )
-                     --}
-  fmap f a = do
-            r <- a
-            return (f r)
-  
+  fmap f (RT sf)
+    = undefined
+      
 instance Applicative Result where
-  pure = return
+  pure  = return
   (<*>) = ap
-  
+
 instance Monad Result where
-  return x = RR (\ env -> return x)
-  
---  (>>=) :: Result a -> (a -> Result b) -> Result b
---              x               f              res
-  x >>= f = RR ( \ env ->
-            do
-                a <- (unRR x) env
-                unRR (f a) env
-            )
-            
-            
--- throwError :: MonadError e m => e -> m a
--- catchError :: MonadError EvalError m => m Result -> (EvalError -> m Result) -> m Result
+  return x
+    = undefined
+
+  RT sf >>= f
+    = undefined
+
 instance MonadError EvalError Result where
   throwError e
-    = RR (\ _ -> throwError e)
-    
-  -- catchError :: Result a -> (EvalError -> Result a) -> Result a
-  catchError (RR m) handler
-    = RR (\ env -> catchError (m env) (\ e -> unRR (handler e) env)
-                -- case m env of
-                -- E e -> unRR (handler e) env
-                -- R x -> return x
-        )
-    
-
--- local :: MonadReader r m => (r -> r) -> m a -> m a
-instance MonadReader Env Result where
-  ask             = RR (\ env -> return env)
-  local f (RR ef) = RR (\ env -> ef (f env))
+    = undefined
   
+  catchError (RT sf) handler
+    = undefined
+
+instance MonadState Store Result where
+  get
+    = undefined
+
+  put st
+    = undefined
+  
+-- ----------------------------------------
+--
+-- variable store
+  
+newtype Store = Store (M.Map Ident Value)
+              deriving (Show)
+
+instance Pretty Store where
+  pretty (Store m)
+    = "{" ++ L.intercalate ", " (map pretty' vars) ++ "}"
+    where
+      pretty' (i, v) = i ++ " :-> " ++ pretty v
+      vars = M.toList m
+      
+emptyStore :: Store
+emptyStore = Store M.empty
+
+lookup :: Ident -> Store -> Maybe Value
+lookup i (Store m) = M.lookup i m
+
+insert :: Ident -> Value -> Store -> Store
+insert i v (Store m) = Store $ M.insert i v m
+
 -- ----------------------------------------
 -- error handling
   
 data EvalError
-  = FreeVar String
+  = UndefVar String
   | NotImpl String
   | ValErr  String Value 
   | Div0
+  | NoLValue Expr
   deriving (Show)
 
 instance Pretty EvalError where
-  pretty (FreeVar i)  = "free variable " ++ show i ++ " in expression"
+  pretty (UndefVar i)  = "undefined variable " ++ show i ++ " in expression"
   pretty (NotImpl n)  = n ++ " not implemented"
   pretty (ValErr e g) = e ++ " value expected, but got: " ++ pretty g
   pretty Div0         = "divide by zero"
-
+  pretty (NoLValue e) = "no lvalue: " ++ pretty e
+  
 boolExpected :: Value -> Result a
 boolExpected = throwError . ValErr "Bool"
 
@@ -152,45 +160,69 @@ intExpected  = throwError . ValErr "Integer"
 notImpl :: String -> Result a
 notImpl = throwError . NotImpl
 
-freeVar :: String -> Result a
-freeVar = throwError . FreeVar
+undefVar :: String -> Result a
+undefVar = throwError . UndefVar
 
 div0 :: Result a
 div0  = throwError Div0
 
+noLValue :: Expr -> Result a
+noLValue = throwError . NoLValue
+
 -- ----------------------------------------
 
-eval' :: Expr -> ResVal Value
-eval' e = (unRR . eval) e M.empty -- start with an empty environment
+eval' :: Expr -> (ResVal Value, Store)
+eval' e = runResult (eval e) emptyStore
 
 eval :: Expr -> Result Value
 eval (BLit b)          = return (B b)
 eval (ILit i)          = return (I i)
-eval (Var    x)        = do
-                            env <- ask 
-                            case M.lookup x env of
-                                (Just v) -> return v
-                                Nothing -> freeVar x
-                                
+
+eval (Var    i)        = undefined
+                         
+eval (Unary preOp e)
+  | preOp `elem` [PreIncr, PreDecr]
+                       = do undefined
+
+eval (Unary postOp e)
+  | postOp `elem` [PostIncr, PostDecr]
+                       = do undefined  -- use evalLValue, readVar, writeVar
+                        
 eval (Unary  op e1)    = do v1  <- eval e1
                             mf1 op v1
 
-eval (Binary op e1 e2) = do v1  <- eval e1
-                            v2  <- eval e2
-                            mf2 op v1 v2
+eval (Binary Assign lhs rhs)
+                       = do undefined  -- use evalLValue, writeVar
+
+eval (Binary Seq e1 e2)
+                       = do undefined
                             
+eval (Binary And e1 e2)
+                       = eval (cond e1 e2 false)
+
+eval (Binary Or  e1 e2)
+                       = undefined -- similar to And
+
+eval (Binary Impl e1 e2)
+                       = undefined -- similar to And
+
+eval (Binary op e1 e2)
+  | isStrict op        = do v1 <- eval e1
+                            v2 <- eval e2
+                            mf2 op v1 v2
+
+eval (Binary op _ _)   = notImpl ("operator " ++ pretty op)
+                         
 eval (Cond   c e1 e2)  = do b <- evalBool c
                             if b
                               then eval e1
                               else eval e2
 
--- let x = e1 in e2
--- let x = True in (x || False)
--- local :: MonadReader r m => (r -> r) -> m a -> m a
-eval (Let x e1 e2) = do
-                        value <- eval e1
-                        local (M.insert x value) (eval e2)
+eval e@(While c body)   = do undefined
 
+eval (Read _)           = notImpl "read"  -- needs IO
+eval (Write _ _)        = notImpl "write" -- needs IO
+                          
 evalBool :: Expr -> Result Bool
 evalBool e
   = do r <- eval e
@@ -198,8 +230,26 @@ evalBool e
         (B b) -> return b
         _     -> boolExpected r
 
+evalLValue :: Expr -> Result Ident
+evalLValue (Var i) = return i
+evalLValue e       = noLValue e
+
+writeVar :: Ident -> Value -> Result ()
+writeVar i v
+  = do st <- get
+       put (insert i v st)
+
+readVar :: Ident -> Result Value
+readVar i
+  = do st <- get
+       case lookup i st of
+        Nothing
+          -> undefVar i
+        Just v
+          -> return v
+
 -- ----------------------------------------
-        
+
 type MF1 = Value -> Result Value
 
 mf1 :: Op1 -> MF1
@@ -208,7 +258,11 @@ mf1 ToInt      = op1BI (toInteger . fromEnum)
 mf1 UPlus      = op1II id
 mf1 UMinus     = op1II (0 -)
 mf1 Signum     = op1II signum
-mf1 op         = \ _ -> notImpl (pretty op)
+mf1 PreIncr    = flip (mf2 Plus ) (I 1)
+mf1 PreDecr    = undefined -- similar to PreIncr
+mf1 PostIncr   = undefined -- similar to PreIncr
+mf1 PostDecr   = undefined -- similar to PreIncr
+-- mf1 op         = \ _ -> notImpl (show op)
   
 op1BB :: (Bool -> Bool) -> MF1
 op1BB op (B b) = return $ B (op b)
@@ -226,6 +280,14 @@ op1BI _  v     = boolExpected v
 
 type MF2 = Value -> Value -> Result Value
 
+isStrict :: Op2 -> Bool
+isStrict op
+  = op `elem`
+    [ Xor, Equiv
+    , Plus, Minus, Mult, Div, Mod
+    , Eq, Neq, Gr, Ge, Ls, Le
+    ]
+    
 mf2 :: Op2 -> MF2
 mf2 And       = op2BBB (&&)
 mf2 Or        = op2BBB (||)
@@ -243,7 +305,7 @@ mf2 Ge        = op2IIB (>=)
 mf2 Gr        = op2IIB (>)
 mf2 Le        = op2IIB (<=)
 mf2 Ls        = op2IIB (<)
-mf2 op        = \ _ _ -> notImpl (pretty op)
+mf2 op        = \ _ _ -> notImpl (show op)
 
 op2BBB :: (Bool -> Bool -> Bool) -> MF2
 op2BBB op (B b1) (B b2) = return (B (b1 `op` b2))
